7c951f28fdfc21e5d4dc38019d099e61615965b9
Strike <Strike@STRIKE-PC>
2020-09-06 18:00:13 -0500

    Creacion de interfaz y previo a la codificacion

---
+++ Project1/Project1/Interfaz.h
@@ -0,0 +1,304 @@
+#pragma once
+
+namespace Project1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Resumen de Interfaz
+	/// </summary>
+	public ref class Interfaz : public System::Windows::Forms::Form
+	{
+	public:
+		Interfaz(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: agregar código de constructor aquí
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		~Interfaz()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^  txtFila;
+	private: System::Windows::Forms::TextBox^  txtColumna;
+	protected:
+
+	protected:
+
+	private: System::Windows::Forms::Label^  lbltitulo;
+	private: System::Windows::Forms::Label^  lblFilas;
+	private: System::Windows::Forms::Label^  lblColumnas;
+	private: System::Windows::Forms::DataGridView^  dgvImagen;
+
+	private: System::Windows::Forms::Panel^  pnEntrada;
+	private: System::Windows::Forms::Label^  lbltitulo2;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Button^  btnMedia;
+	private: System::Windows::Forms::Button^  btnMediana;
+	private: System::Windows::Forms::Button^  btnLaplaciano;
+	private: System::Windows::Forms::Button^  btnSobel;
+	private: System::Windows::Forms::Label^  lblTitulo3;
+	private: System::Windows::Forms::Label^  lblTitulo4;
+	private: System::Windows::Forms::DataGridView^  dataGridView1;
+	private: System::Windows::Forms::Button^  btnOtraImagen;
+	private: System::Windows::Forms::Button^  button2;
+	private: System::Windows::Forms::Panel^  pnIngreso;
+
+
+	private:
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->txtFila = (gcnew System::Windows::Forms::TextBox());
+			this->txtColumna = (gcnew System::Windows::Forms::TextBox());
+			this->lbltitulo = (gcnew System::Windows::Forms::Label());
+			this->lblFilas = (gcnew System::Windows::Forms::Label());
+			this->lblColumnas = (gcnew System::Windows::Forms::Label());
+			this->dgvImagen = (gcnew System::Windows::Forms::DataGridView());
+			this->pnEntrada = (gcnew System::Windows::Forms::Panel());
+			this->lbltitulo2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->btnMedia = (gcnew System::Windows::Forms::Button());
+			this->btnMediana = (gcnew System::Windows::Forms::Button());
+			this->btnLaplaciano = (gcnew System::Windows::Forms::Button());
+			this->btnSobel = (gcnew System::Windows::Forms::Button());
+			this->lblTitulo3 = (gcnew System::Windows::Forms::Label());
+			this->lblTitulo4 = (gcnew System::Windows::Forms::Label());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->btnOtraImagen = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->pnIngreso = (gcnew System::Windows::Forms::Panel());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dgvImagen))->BeginInit();
+			this->pnEntrada->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->pnIngreso->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// txtFila
+			// 
+			this->txtFila->Location = System::Drawing::Point(121, 40);
+			this->txtFila->Name = L"txtFila";
+			this->txtFila->Size = System::Drawing::Size(88, 20);
+			this->txtFila->TabIndex = 0;
+			// 
+			// txtColumna
+			// 
+			this->txtColumna->Location = System::Drawing::Point(331, 40);
+			this->txtColumna->Name = L"txtColumna";
+			this->txtColumna->Size = System::Drawing::Size(96, 20);
+			this->txtColumna->TabIndex = 1;
+			// 
+			// lbltitulo
+			// 
+			this->lbltitulo->AutoSize = true;
+			this->lbltitulo->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lbltitulo->Location = System::Drawing::Point(13, 3);
+			this->lbltitulo->Name = L"lbltitulo";
+			this->lbltitulo->Size = System::Drawing::Size(258, 19);
+			this->lbltitulo->TabIndex = 2;
+			this->lbltitulo->Text = L"Ingrese las dimensiones de la imagen";
+			// 
+			// lblFilas
+			// 
+			this->lblFilas->AutoSize = true;
+			this->lblFilas->Location = System::Drawing::Point(84, 43);
+			this->lblFilas->Name = L"lblFilas";
+			this->lblFilas->Size = System::Drawing::Size(31, 13);
+			this->lblFilas->TabIndex = 3;
+			this->lblFilas->Text = L"Filas:";
+			// 
+			// lblColumnas
+			// 
+			this->lblColumnas->AutoSize = true;
+			this->lblColumnas->Location = System::Drawing::Point(269, 43);
+			this->lblColumnas->Name = L"lblColumnas";
+			this->lblColumnas->Size = System::Drawing::Size(56, 13);
+			this->lblColumnas->TabIndex = 4;
+			this->lblColumnas->Text = L"Columnas:";
+			// 
+			// dgvImagen
+			// 
+			this->dgvImagen->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dgvImagen->Location = System::Drawing::Point(121, 83);
+			this->dgvImagen->Name = L"dgvImagen";
+			this->dgvImagen->Size = System::Drawing::Size(287, 254);
+			this->dgvImagen->TabIndex = 5;
+			// 
+			// pnEntrada
+			// 
+			this->pnEntrada->Controls->Add(this->dgvImagen);
+			this->pnEntrada->Controls->Add(this->lblColumnas);
+			this->pnEntrada->Controls->Add(this->lblFilas);
+			this->pnEntrada->Controls->Add(this->lbltitulo);
+			this->pnEntrada->Controls->Add(this->txtColumna);
+			this->pnEntrada->Controls->Add(this->txtFila);
+			this->pnEntrada->Location = System::Drawing::Point(109, 8);
+			this->pnEntrada->Name = L"pnEntrada";
+			this->pnEntrada->Size = System::Drawing::Size(510, 356);
+			this->pnEntrada->TabIndex = 6;
+			// 
+			// lbltitulo2
+			// 
+			this->lbltitulo2->AutoSize = true;
+			this->lbltitulo2->Location = System::Drawing::Point(103, 12);
+			this->lbltitulo2->Name = L"lbltitulo2";
+			this->lbltitulo2->Size = System::Drawing::Size(188, 13);
+			this->lbltitulo2->TabIndex = 7;
+			this->lbltitulo2->Text = L"Ingrese las dimensiones de la máscara";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(319, 9);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 8;
+			// 
+			// btnMedia
+			// 
+			this->btnMedia->Location = System::Drawing::Point(81, 83);
+			this->btnMedia->Name = L"btnMedia";
+			this->btnMedia->Size = System::Drawing::Size(75, 23);
+			this->btnMedia->TabIndex = 9;
+			this->btnMedia->Text = L"Media";
+			this->btnMedia->UseVisualStyleBackColor = true;
+			// 
+			// btnMediana
+			// 
+			this->btnMediana->Location = System::Drawing::Point(200, 83);
+			this->btnMediana->Name = L"btnMediana";
+			this->btnMediana->Size = System::Drawing::Size(75, 23);
+			this->btnMediana->TabIndex = 10;
+			this->btnMediana->Text = L"Mediana";
+			this->btnMediana->UseVisualStyleBackColor = true;
+			// 
+			// btnLaplaciano
+			// 
+			this->btnLaplaciano->Location = System::Drawing::Point(315, 83);
+			this->btnLaplaciano->Name = L"btnLaplaciano";
+			this->btnLaplaciano->Size = System::Drawing::Size(75, 23);
+			this->btnLaplaciano->TabIndex = 11;
+			this->btnLaplaciano->Text = L"Laplaciano";
+			this->btnLaplaciano->UseVisualStyleBackColor = true;
+			// 
+			// btnSobel
+			// 
+			this->btnSobel->Location = System::Drawing::Point(432, 83);
+			this->btnSobel->Name = L"btnSobel";
+			this->btnSobel->Size = System::Drawing::Size(75, 23);
+			this->btnSobel->TabIndex = 12;
+			this->btnSobel->Text = L"Sobel";
+			this->btnSobel->UseVisualStyleBackColor = true;
+			// 
+			// lblTitulo3
+			// 
+			this->lblTitulo3->AutoSize = true;
+			this->lblTitulo3->Location = System::Drawing::Point(22, 49);
+			this->lblTitulo3->Name = L"lblTitulo3";
+			this->lblTitulo3->Size = System::Drawing::Size(132, 13);
+			this->lblTitulo3->TabIndex = 13;
+			this->lblTitulo3->Text = L"Seleccione un tipo de filtro";
+			// 
+			// lblTitulo4
+			// 
+			this->lblTitulo4->AutoSize = true;
+			this->lblTitulo4->Location = System::Drawing::Point(22, 142);
+			this->lblTitulo4->Name = L"lblTitulo4";
+			this->lblTitulo4->Size = System::Drawing::Size(96, 13);
+			this->lblTitulo4->TabIndex = 14;
+			this->lblTitulo4->Text = L"Imagen Resultante";
+			this->lblTitulo4->Click += gcnew System::EventHandler(this, &Interfaz::lblTitulo4_Click);
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(181, 159);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(284, 228);
+			this->dataGridView1->TabIndex = 15;
+			// 
+			// btnOtraImagen
+			// 
+			this->btnOtraImagen->Location = System::Drawing::Point(43, 309);
+			this->btnOtraImagen->Name = L"btnOtraImagen";
+			this->btnOtraImagen->Size = System::Drawing::Size(113, 28);
+			this->btnOtraImagen->TabIndex = 16;
+			this->btnOtraImagen->Text = L"Elegir otra imagen";
+			this->btnOtraImagen->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(43, 348);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(111, 29);
+			this->button2->TabIndex = 17;
+			this->button2->Text = L"Elegir otro filtro";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// pnIngreso
+			// 
+			this->pnIngreso->Controls->Add(this->button2);
+			this->pnIngreso->Controls->Add(this->btnOtraImagen);
+			this->pnIngreso->Controls->Add(this->dataGridView1);
+			this->pnIngreso->Controls->Add(this->lblTitulo4);
+			this->pnIngreso->Controls->Add(this->lblTitulo3);
+			this->pnIngreso->Controls->Add(this->btnSobel);
+			this->pnIngreso->Controls->Add(this->btnLaplaciano);
+			this->pnIngreso->Controls->Add(this->btnMediana);
+			this->pnIngreso->Controls->Add(this->btnMedia);
+			this->pnIngreso->Controls->Add(this->textBox1);
+			this->pnIngreso->Controls->Add(this->lbltitulo2);
+			this->pnIngreso->Location = System::Drawing::Point(636, 12);
+			this->pnIngreso->Name = L"pnIngreso";
+			this->pnIngreso->Size = System::Drawing::Size(566, 399);
+			this->pnIngreso->TabIndex = 18;
+			this->pnIngreso->Visible = false;
+			// 
+			// Interfaz
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
+			this->ClientSize = System::Drawing::Size(704, 417);
+			this->Controls->Add(this->pnIngreso);
+			this->Controls->Add(this->pnEntrada);
+			this->Name = L"Interfaz";
+			this->Text = L"Interfaz";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dgvImagen))->EndInit();
+			this->pnEntrada->ResumeLayout(false);
+			this->pnEntrada->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->pnIngreso->ResumeLayout(false);
+			this->pnIngreso->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void lblTitulo4_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+};
+}
--- Project1/Project1/MyForm.h
+++ Project1/Project1/MyForm.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include<iostream>
+#include"Interfaz.h"
 namespace Project1 {
 
 	using namespace System;
@@ -35,6 +36,14 @@ namespace Project1 {
 			}
 		}
 	private: System::Windows::Forms::Label^  lblTitulo;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Button^  btnInicio;
+	private: System::Windows::Forms::Label^  lblGrupo;
+	private: System::Windows::Forms::PictureBox^  pictureBox2;
+
+
+
+
 	protected:
 
 	private:
@@ -50,33 +59,104 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->lblTitulo = (gcnew System::Windows::Forms::Label());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->btnInicio = (gcnew System::Windows::Forms::Button());
+			this->lblGrupo = (gcnew System::Windows::Forms::Label());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// lblTitulo
 			// 
 			this->lblTitulo->AutoSize = true;
-			this->lblTitulo->Font = (gcnew System::Drawing::Font(L"Cooper Black", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->lblTitulo->Font = (gcnew System::Drawing::Font(L"Cooper Black", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->lblTitulo->ForeColor = System::Drawing::Color::Blue;
-			this->lblTitulo->Location = System::Drawing::Point(75, 9);
+			this->lblTitulo->Location = System::Drawing::Point(116, 22);
 			this->lblTitulo->Name = L"lblTitulo";
-			this->lblTitulo->Size = System::Drawing::Size(467, 27);
+			this->lblTitulo->Size = System::Drawing::Size(397, 68);
 			this->lblTitulo->TabIndex = 0;
-			this->lblTitulo->Text = L"FILTRADO ESPACIAL DE IMÁGENES";
+			this->lblTitulo->Text = L"FILTRADO ESPACIAL DE \r\n              IMÁGENES";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(519, -16);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(143, 134);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click_1);
+			// 
+			// btnInicio
+			// 
+			this->btnInicio->BackColor = System::Drawing::Color::LightGreen;
+			this->btnInicio->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
+			this->btnInicio->Font = (gcnew System::Drawing::Font(L"Lucida Bright", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btnInicio->Location = System::Drawing::Point(317, 332);
+			this->btnInicio->Name = L"btnInicio";
+			this->btnInicio->Size = System::Drawing::Size(107, 28);
+			this->btnInicio->TabIndex = 2;
+			this->btnInicio->Text = L" INICIAR";
+			this->btnInicio->UseVisualStyleBackColor = false;
+			this->btnInicio->Click += gcnew System::EventHandler(this, &MyForm::btnInicio_Click);
+			// 
+			// lblGrupo
+			// 
+			this->lblGrupo->AutoSize = true;
+			this->lblGrupo->Font = (gcnew System::Drawing::Font(L"Britannic Bold", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lblGrupo->Location = System::Drawing::Point(200, 332);
+			this->lblGrupo->Name = L"lblGrupo";
+			this->lblGrupo->Size = System::Drawing::Size(99, 27);
+			this->lblGrupo->TabIndex = 3;
+			this->lblGrupo->Text = L"Grupo 3";
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(113, 102);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(400, 214);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox2->TabIndex = 4;
+			this->pictureBox2->TabStop = false;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(646, 341);
+			this->AutoSize = true;
+			this->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
+			this->ClientSize = System::Drawing::Size(657, 372);
+			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->lblGrupo);
+			this->Controls->Add(this->btnInicio);
+			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->lblTitulo);
 			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::WindowsDefaultBounds;
 			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void pictureBox1_Click_1(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void btnInicio_Click(System::Object^  sender, System::EventArgs^  e) {
+		Interfaz^entrada = gcnew Interfaz();
+		entrada->ShowDialog();
+		//this->Close();
+	}
+};
 }
